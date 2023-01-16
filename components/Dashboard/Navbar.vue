e9fd7167979ca71babc068250731aea1cd610486
mandragora-hub <royer01gomez@gmail.comn>
2023-01-15 20:58:09 -0400

    feat: add search bar

--- components/Dashboard/Navbar.vue
+++ components/Dashboard/Navbar.vue
@@ -3,13 +3,7 @@
     <template #menu>
       <div class="relative hidden lg:flex items-center ml-auto">
         <div class="flex items-center justify-center">
-          <img
-            class="w-6 h-6 rounded-full"
-            src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
-            alt="Avatar of Jonathan Reinink"
-          />
-          <span class="ml-2 text-sm font-semibold">Alfian</span>
-          <IconUil:angle-down />
+          <SearchBar />
         </div>
         <div
           class="flex space-x-4 border-l ml-6 pl-6 border-gray-900/10 dark:border-gray-50/[0.2]"
